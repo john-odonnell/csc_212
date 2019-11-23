023eb9c1fe59c7de3c04398bb5459c2113f335a7
john-odonnell <johnodonnell77@my.uri.edu>
2019-11-23 00:53:53 -0500

    lab11 complete

---
+++ labs/lab11/getbooks.sh
@@ -0,0 +1,3 @@
+curl -o alice.txt http://www.gutenberg.org/files/28885/28885-8.txt
+curl -o pride.txt https://www.gutenberg.org/files/1342/1342-0.txt
+curl -o war.txt https://www.gutenberg.org/files/2600/2600-0.txt

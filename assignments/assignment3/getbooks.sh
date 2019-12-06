f07cb72843e374aaa2b666d3ea20662a0b5d0a99
john-odonnell <johnodonnell77@my.uri.edu>
2019-12-06 15:22:51 -0500

    assignment3 in progress. 60/100

---
+++ assignments/assignment3/getbooks.sh
@@ -0,0 +1,3 @@
+curl -o crime.txt https://www.gutenberg.org/files/2554/2554-0.txt
+curl -o wild.txt https://www.gutenberg.org/files/215/215-0.txt
+curl -o miserable.txt https://www.gutenberg.org/files/135/135-0.txt
